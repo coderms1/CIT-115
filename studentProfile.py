6c80a096439c6f9907ef0ec300507affbda64d96
MS1 <157831131+coderms1@users.noreply.github.com>
2026-09-22 10:32:40 -0400

    Add student profile input and display functionality

---
+++ studentProfile.py
@@ -0,0 +1,18 @@
+# Project: Student Profile
+# Author: (Your name here)
+# Class: Python 1
+# Due: 9/20/26
+
+name = input("Name: ")
+program = input("Program of Study: ")
+tech = input("Favorite Tech: ")
+band = input("Favorite Band: ")
+
+print()
+print("Student Profile")
+print("-------------")
+print(f"Name: {name}")
+print(f"Program: {program}")
+print(f"Favorite Tech: {tech}")
+print(f"Favorite Band: {band}")
+print("-------------")
