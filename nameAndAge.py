f36ea1d8bd5c789f2bf066031c69f6edbc071620
MS1 <157831131+coderms1@users.noreply.github.com>
2026-09-22 10:31:42 -0400

    Add name and age input/output functionality

---
+++ nameAndAge.py
@@ -0,0 +1,11 @@
+# Project: Name and Age
+# Author: (Your name here)
+# Class: Python 1
+# Due: 9/24/26
+
+# INPUT (prompt user)
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# OUTPUT
+print(f"Hello, {name}. You are {age} years old.")
